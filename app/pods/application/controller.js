5c8ea0a315a4d242067139d1a2d4dfa3c345f57f
Seena <seenarowhani95@gmail.com>
2016-10-09 17:36:01 -0400

    repo init

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,25 +1,11 @@
 import Ember from 'ember';
 const {
   Controller,
-  get,
-  set,
-  observer,
   computed: {
     alias
-  },
-  $: {
-    ajax
   }
 } = Ember
+
 export default Controller.extend({
-  queryParams: ['page'],
-  repos: null,
-  page: 1,
-  pageObserver: Ember.observer('page', function () {
-    const page = get(this, 'page')
-    const url = `http://api.github.com/orgs/ciena-frost/repos?page=${page}&per_page=10`
-    return ajax(url).then((repos) => {
-      set(this, 'repos', repos)
-    })
-  }).on('init')
+  tags: alias('model.tags')
 });
--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,5 +1,29 @@
 import Ember from 'ember';
 const {
-  Route
+  Route,
+  RSVP: {
+    hash
+  }
 } = Ember
-export default Route.extend({});
+export default Route.extend({
+  model () {
+    return hash({
+      repo: $.get('https://api.github.com/repos/DankIdeaCentral/migrate-app'),
+      pulls: $.get('https://api.github.com/repos/DankIdeaCentral/migrate-app/pulls'),
+      tags: $.get('https://api.github.com/repos/DankIdeaCentral/migrate-app/tags').then(function (tags) {
+        return Promise.all(tags.map(function (tag) {
+          return new Promise(function (resolve, reject) {
+            let commits = tag.commit.url
+            resolve($.get({
+              url: commits,
+              async: false
+            }))
+          }).then(commit => {
+            tag.commit = commit;
+            return tag
+          })
+        }))
+    })
+  })
+  }
+});
