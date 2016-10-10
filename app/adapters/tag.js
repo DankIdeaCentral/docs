adb140e4e968f5726f05cb370f545555892b45eb
Seena <seenarowhani95@gmail.com>
2016-10-10 17:59:56 -0400

    mod base adapter

--- app/adapters/tag.js
+++ app/adapters/tag.js
@@ -1,8 +1,3 @@
 import BaseAdapter from './base'
 
-export default BaseAdapter.extend({
-  buildURL (modelName, id, snapshot, requestType, query) {
-    let result = this._super(...arguments)
-    return `${result}?page=${query.page}&per_page=${query.per_page}`
-  }
-})
+export default BaseAdapter
